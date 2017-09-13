3ae7e9462fe55eb019a273b624f04541b6b59543
wida <wida@foxmail.com>
2017-09-13 10:32:47 +0800

    完成wal 和snapshot

--- cmd/server.go
+++ cmd/server.go
@@ -5,14 +5,32 @@ import (
 	"github.com/widaT/newredis"
 	"flag"
 	"fmt"
+	"net/http"
+	_  "net/http/pprof"
 )
 
+const VERSION = "newredis v0.1"
 //var addr = ":6380"
 func main() {
 	s := flag.Uint64("s",1000000,"snapshot count")
 	w :=flag.String("w","aw","use wal to save data to disk,es every second ,al allways,no no use wal")
-	p :=flag.Int("p",6380,"net port")
+	p :=flag.Int("p",6380,"port for net listen")
+	v :=flag.String("v","","show the version")
+	P :=flag.Bool("P",false,"profiling this program")
 	flag.Parse()
+
+	if v != nil {
+		fmt.Println(VERSION)
+		return
+	}
+
+
+	if P {
+		go func() {
+			http.ListenAndServe("localhost:6060", nil)
+		}()
+	}
+
 	c := newredis.DefaultConfig().SnapCount(*s).OpenWal(*w).Laddr(fmt.Sprintf(":%d",*p))
 	go log.Printf("started server at %s", c.Gaddr())
 	err := newredis.ListenAndServe(c,
