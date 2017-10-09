c74ec86abb73b02eec5fbd66aa46b1d3b603a33b
wida <wida@foxmail.com>
2017-10-09 15:50:09 +0800

    es 加锁

--- cmd/server.go
+++ cmd/server.go
@@ -12,12 +12,10 @@ import (
 const VERSION = "newredis v0.1"
 func main() {
 	s := flag.Uint64("s",1000000,"snapshot count")
-	//@todo 可以尝试下every second
 	w :=flag.String("w","aw","use wal to save data to disk al allways,es every second ,no no use wal")
 	p :=flag.Int("p",6380,"port for net listen")
 	P :=flag.Bool("P",false,"profiling this program")
 	flag.Parse()
-
 
 	if flag.Arg(0) == "version" {
 		fmt.Println(VERSION)
