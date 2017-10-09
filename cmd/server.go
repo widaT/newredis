69c3921eac4171b26ce9c3f23b4f648c793cd1fe
wida <wida@foxmail.com>
2017-10-09 16:21:19 +0800

    es 加锁

--- cmd/server.go
+++ cmd/server.go
@@ -7,12 +7,14 @@ import (
 	"fmt"
 	"net/http"
 	_  "net/http/pprof"
+	"os"
 )
 
 const VERSION = "newredis v0.1"
 func main() {
 	s := flag.Uint64("s",1000000,"snapshot count")
 	w :=flag.String("w","aw","use wal to save data to disk al allways,es every second ,no no use wal")
+	d :=flag.String("data","data/","dir to save wal and snapshot")
 	p :=flag.Int("p",6380,"port for net listen")
 	P :=flag.Bool("P",false,"profiling this program")
 	flag.Parse()
@@ -26,10 +28,17 @@ func main() {
 			http.ListenAndServe("localhost:6060", nil)
 		}()
 	}
+	dirpath  := *d
+	_, err := os.Stat(dirpath)
+	if err != nil {
+		if err := os.Mkdir(dirpath, 0750); err != nil {
+			log.Fatalf("raft-redis: cannot create dir for wal (%v)", err)
+		}
+	}
 
-	c := newredis.DefaultConfig().SnapCount(*s).OpenWal(*w).Laddr(fmt.Sprintf(":%d",*p))
+	c := newredis.DefaultConfig().SnapCount(*s).OpenWal(*w).Laddr(fmt.Sprintf(":%d",*p)).DataDir(dirpath)
 	go log.Printf("started server at %s wal model %s", c.Gaddr(),c.Gwalsavetype())
-	err := newredis.ListenAndServe(c,
+	err = newredis.ListenAndServe(c,
 		func(conn newredis.Conn) bool {
 			return true
 		},
